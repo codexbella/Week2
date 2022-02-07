e414b422ae9d955e4ae6089bddb75c8c8df3ae53
Anna Lena Baumann <rescht@posteo.de>
2022-02-07 17:05:20 +0100

    constructors and overrides implemented

--- src/main/java/de/codexbella/day1/Student.java
+++ src/main/java/de/codexbella/day1/Student.java
@@ -1,10 +1,28 @@
 package de.codexbella.day1;
 
+import java.util.Objects;
+
 public class Student {
-    private String firstName = "";//oder: private String firstName; (ohne Initialisierung, aber dann wird bei Strings kein Wert, also null, gesetzt.)
-    private String lastName = "";//oder: private String lastName;
+    private String lastName = "";//oder: private String lastName; (ohne Initialisierung, aber dann wird bei Strings kein Wert, also null, gesetzt.)
+    private String firstName = "";//oder: private String firstName;
     private String studentNumber = "00000000";//oder: private String studentNumber;
     private int currentSemester = 1;//oder: private in currentSemester;
+
+    //Konstruktor:
+    Student(String lastName, String firstName, String studentNumber, int currentSemester){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+        this.currentSemester = currentSemester;
+    }
+
+    //Häufiger Konstruktor ist einer zum sich selbst Kopieren:
+    Student(Student toCopy) {
+        this.firstName = toCopy.getFirstName();
+        this.lastName = toCopy.getLastName();
+        this.studentNumber = toCopy.getStudentNumber();
+        this.currentSemester = toCopy.getCurrentSemester();
+    }
 
     public String getFirstName() {
         return firstName;
@@ -29,5 +47,25 @@ public class Student {
     }
     public void setCurrentSemester(int currentSemesterInput) {
         this.currentSemester = currentSemesterInput;
+    }
+
+    @Override
+    public String toString() {
+        return getLastName() + ", " + getFirstName() + " - Student number: " + getStudentNumber() + ", current semester: " + getCurrentSemester();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        return currentSemester == student.currentSemester && Objects.equals(lastName, student.lastName) && Objects.equals(firstName, student.firstName) && Objects.equals(studentNumber, student.studentNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName, firstName, studentNumber, currentSemester);
     }
 }
--- src/test/java/de/codexbella/day1/StudentTest.java
+++ src/test/java/de/codexbella/day1/StudentTest.java
@@ -5,26 +5,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
-
-    Student student1 = new Student();
     @Test
     void testSetAndGetFirstName() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
         student1.setFirstName("Karla");
         assertEquals("Karla", student1.getFirstName());
     }
     @Test
     void testSetAndGetLastName() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
         student1.setLastName("Musterfrau");
         assertEquals("Musterfrau", student1.getLastName());
     }
     @Test
     void testSetAndGetStudentNumber() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
         student1.setStudentNumber("80907060");
         assertEquals("80907060", student1.getStudentNumber());
     }
     @Test
     void testSetAndGetCurrentSemester() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
         student1.setCurrentSemester(3);
         assertEquals(3, student1.getCurrentSemester());
+    }
+    @Test
+    void testGetStudentInfo() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
+        assertEquals("Musterfrau, Karla - Student number: 80907060, current semester: 3", student1.toString());
+    }
+    @Test
+    void testCopy() {
+        var student = new Student("Schreck", "André", "50406030", 11);
+        var result = new Student(student);
+
+        assertEquals("André", result.getFirstName());
+        assertEquals("Schreck", result.getLastName());
+        assertEquals("50406030", result.getStudentNumber());
+        assertEquals(11, result.getCurrentSemester());
+    }
+    @Test
+    void testEqualsShouldBeDifferent() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
+        Student student2 = new Student("Mustermensch", "Karl", "80907061", 1);
+        assertEquals(false, student1.equals(student2));
+    }
+    @Test
+    void testEqualsShouldBeTheSame() {
+        Student student1 = new Student("Musterfrau", "Karla", "80907060", 3);
+        Student student2 = new Student("Musterfrau", "Karla", "80907060", 3);
+        assertEquals(true, student1.equals(student2));
     }
 }
